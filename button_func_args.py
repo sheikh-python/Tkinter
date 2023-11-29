d476398470dc6343b78309c084af3edaed742e27
Ahmad sheikh zadeh <142123831+sheikh-python@users.noreply.github.com>
2023-11-29 23:17:28 +0330

    lesson 5

---
+++ button_func_args.py
@@ -0,0 +1,29 @@
+import tkinter as tk
+from tkinter import ttk
+
+def button_func(entry_string):
+    print('a button was pressed')
+    print(entry_string.get())
+
+def outer_func(parameter):
+    def inner_func():
+        print('button pressed')
+        print(parameter.get())
+    return inner_func
+# setup
+window = tk.Tk()
+window.title('buttons, functions and arguments')
+window.geometry('600x400')
+
+#widgets 
+entry_string = tk.StringVar(value='test')
+entry = ttk.Entry(window , textvariable=entry_string)
+entry.pack()
+
+button = ttk.Button(window, text='button ' , command = outer_func(entry_string)  )
+
+button.pack()
+
+
+# run
+window.mainloop()
